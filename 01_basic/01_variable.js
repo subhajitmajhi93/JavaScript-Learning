a45ee21b39a33284a137919d77da8b10ba2fcc11
subhajitmajhi93 <subhajitmajhioslo@gmail.com>
2026-06-07 20:09:47 +0000

    commit variable note

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,14 @@
+const accountId=425678
+let accountEmail="subhajit.majhi@gmail.com"
+var accoundPassword="subhajit"
+accountCity="Kolkata"
+let accountstate
+//accountId=2
+console.log(accountId);
+/* prefer not to use var 
+becasue of issue in block scope and funcational scope
+*/
+
+console.table([accountEmail,accoundPassword,accountId,accountCity,accountstate]);
+
+
