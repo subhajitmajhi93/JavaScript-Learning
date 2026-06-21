a77cb3fc8f10e69165ce142ff727c0741d5b3159
subhajitmajhi93 <subhajitmajhioslo@gmail.com>
2026-06-21 08:34:39 +0000

    conversionType discussed

---
+++ 01_basic/conversion.js
@@ -0,0 +1,16 @@
+let score="Hitesh"
+console.log(typeof score);
+console.log(typeof (score));
+
+// there is no need to write new 
+let valueInNumber= Number(score);
+console.log(typeof(valueInNumber));
+console.log(valueInNumber);
+//"33"--->33
+//"33abc"--->NAN
+//true-->1 false-->0
+let isLogged="HItesh";
+let isLogggerValue= Boolean(isLogged)
+console.log(isLogggerValue);
+// 1==>true 0-->false
+//"HItesh"-->false
